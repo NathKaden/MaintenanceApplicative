204b413d95dec5dab041e1929bb320626737a507
NathKaden <boulangeot.nathan@gmail.com>
2025-02-10 16:51:56 +0100

    Update Customer

--- src/main/java/fr/r6a06/Customer.java
+++ src/main/java/fr/r6a06/Customer.java
@@ -28,19 +28,15 @@ public class Customer {
 
         String result = "Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            //determine amounts for each line
-            // Il faut demander à rentals le prix
-            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                     each.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         //add footer lines
